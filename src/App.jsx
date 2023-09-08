24c489db416f4fb0bdd6a7ca2ea7f4ce236a3ca0
phyxius23 <30ntin@gmail.com>
2023-09-08 14:31:28 +0200

    fixed react-device-detect

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { isMobile } from "react-device-detect";
 import Home from "./components/Home";
 import Search from "./components/Search";
 import DailyForecast from "./components/DailyForecast";
 import DetailedForecast from "./components/DetailedForecast";
 
 function App() {
-	return (
+	return isMobile ? (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Home />} />
@@ -14,6 +15,8 @@ function App() {
 				<Route path="/city/:city/detail/:lastUpdate" element={<DetailedForecast />} />
 			</Routes>
 		</BrowserRouter>
+	) : (
+		<p>App visualizzabile solo su mobile</p>
 	);
 }
 
